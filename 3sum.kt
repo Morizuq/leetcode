ce0ad3511f4a1d9007b2fa69bf928c9b3eb24fb6
Morizuq Shoneye <morizuqshoneye@gmail.com>
2026-04-30 02:45:52 +0100

    3sum solved with two pointer

---
+++ 3sum.kt
@@ -0,0 +1,35 @@
+class Solution {
+    fun threeSum(nums: IntArray): List<List<Int>> {
+        val result = mutableListOf<List<Int>>()
+
+        nums.sort()
+
+        for(i in 0 until nums.size - 2){
+
+            if (i > 0 && nums[i] == nums[i - 1]) continue
+
+            var left = i + 1
+            var right = nums.size - 1
+
+            while(left < right){
+                val sum = nums[i] + nums[left] + nums[right]
+                when {
+                    sum == 0 -> {
+                        result.add(listOf(nums[i], nums[left], nums[right]))
+                        left++
+                        right--
+                        
+                        // Checking for duplicates
+                        while(left < right && nums[left] == nums[left -1]) left++
+                        while(left < right && nums[right] == nums[right + 1]) right--
+                    }
+                    // Lower sum, move the left pointer forward
+                    sum < 0 -> left++
+                    else -> right--
+                } 
+            }
+        }
+
+        return result
+    }
+}
